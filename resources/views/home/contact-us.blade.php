123daf8834ebe0ad79054254d36d2dee005f188a
morteza emamverdi <morteza.emamverdi1372@gmail.com>
2023-07-09 22:43:40 +0330

    insert leaflet for contact us page

--- resources/views/home/contact-us.blade.php
+++ resources/views/home/contact-us.blade.php
@@ -5,30 +5,34 @@
 @endsection
 
 @section('style')
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossorigin=""/>
 @endsection
 
-@section('script')
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-            integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-            crossorigin="">
-    </script>
+@section('scripts')
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
     <script>
-        var mymap = L.map('map').setView([{{ $setting->latitude }}, {{ $setting->longitude }}], 12);
+        var map = L.map('map').setView([{{$setting->longitude}}, {{ $setting->latitude}}], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-        }).addTo(mymap);
-        var marker = L.marker([{{ $setting->latitude }}, {{ $setting->longitude }}]).addTo(mymap);
+        var marker = L.marker([{{$setting->longitude}}, {{ $setting->latitude}}]).addTo(map);
+
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        circle.bindPopup("I am a circle.");
+        polygon.bindPopup("I am a polygon.");
+        {{--var popup = L.popup()--}}
+        {{--    .setLatLng([{{$setting->longitude}}, {{ $setting->latitude}}])--}}
+        {{--    .setContent("I am a standalone popup.")--}}
+        {{--    .openOn(map);--}}
+
     </script>
+
 @endsection
 
 @section('content')
